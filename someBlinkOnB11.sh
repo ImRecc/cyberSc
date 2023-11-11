637046f1dc8b50e55986f4b12d183fd62ce796f9
ImRecc <46367069+ImRecc@users.noreply.github.com>
2023-11-11 02:11:47 -0500

    Update and rename someBlinkOnB11 to someBlinkOnB11.sh

--- someBlinkOnB11.sh
+++ someBlinkOnB11.sh
@@ -4,3 +4,9 @@ I have tried to figure out if there is *.list.txt or *.mp3 or folder..
 and I believe that by the name of our Network Sam, there must be a .key file or psw file in the sever...
 
 directory-level configuration file .. Apache..
+
+we tried every where through the .htaccess file
+but when we occasionally spotted the web directory scanner like dirBurster, we do find /b11/e/l/t/o/n/.htaccess 
+"the password is somewhere closer! try harder?"
+
+.. yes, somewhere closer!
