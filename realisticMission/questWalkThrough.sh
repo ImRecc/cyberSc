0eddbb8429dbe836cc24194a3d659347c905d39c
ImRecc <46367069+ImRecc@users.noreply.github.com>
2023-12-15 19:44:35 +0800

    Update questWalkThrough.sh

--- realisticMission/questWalkThrough.sh
+++ realisticMission/questWalkThrough.sh
@@ -1,6 +1,10 @@
 #!/hackThisSite/realisticMission
 
-# R1. simple injection attack
+R1. simple injection attack
 
 R2. gobuster or some other scanner\fuzzer...
     requires me to gain admin access, so must be an login.php or somthing similar...
+    we got a login page, and tried SQL injection, commentalized "request password"
+    
+
+R3. yes, goBuster, but nothing interesting found 403 code haha..
