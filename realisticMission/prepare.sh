007a637269a7808e5643867d267ced4b4da67295
ImRecc <46367069+ImRecc@users.noreply.github.com>
2023-12-17 18:50:51 +0800

    Create prepare.sh

---
+++ realisticMission/prepare.sh
@@ -0,0 +1,6 @@
+!#/hts/realistic/3/
+
+oldindex.html
+submit sector, ssi injection?
+common one are not respone and may not been executed from server side.. 
+<!#exec command ='ls'> just been uploaded and nothing special happen, no running no error check...
