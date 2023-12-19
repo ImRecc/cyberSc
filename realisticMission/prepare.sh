1c9ebf75a89b3bb98a69564048afff2f91570108
ImRecc <46367069+ImRecc@users.noreply.github.com>
2023-12-19 19:53:31 +0800

    Update prepare.sh

--- realisticMission/prepare.sh
+++ realisticMission/prepare.sh
@@ -4,3 +4,7 @@ oldindex.html
 submit sector, ssi injection?
 common one are not respone and may not been executed from server side.. 
 <!#exec command ='ls'> just been uploaded and nothing special happen, no running no error check...
+
+fine, I have to learn more about two injection techniques, and the directory traversal either
+
+for the traversal, still no go on it cause the lucky part not come yet
