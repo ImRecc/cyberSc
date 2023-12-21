ae3964b60599b3516ecc6f8e401277231b14ce2b
ImRecc <46367069+ImRecc@users.noreply.github.com>
2023-12-21 19:25:06 +0800

    Hack this site (#7)
    * Update README.md
    
    * Create interestQuestInBasic
    
    * Delete aLittleBitBash
    
    * Delete banditMp7.txt
    
    * Create someBlinkOnB11
    
    * Update and rename someBlinkOnB11 to someBlinkOnB11.sh
    
    * Update someBlinkOnB11.sh
    
    * Update README.md
    
    * Rename interestQuestInBasic to basicMission/interestQuestInBasic
    
    * Rename someBlinkOnB11.sh to basicMission/someBlinkOnB11.sh
    
    * Create questWalkThrough.sh
    
    * Update questWalkThrough.sh
    
    * Create prepare.sh
    
    * Update prepare.sh
    
    * Create r3-injections.sh
    
    * Update prepare.sh
    
    * Update questWalkThrough.sh
    
    * Create R3. probs and walk path
    
    * moved to /when Stucked/R3.probs & walk path
    
    * moved to /when Stucked/R3.probs & walk path
    
    * Update and rename R3. probs and walk path to R3. probs and walk path.sh

---
+++ basicMission/someBlinkOnB11.sh
@@ -0,0 +1,13 @@
+#!/hackThisSite.org/basic/11
+
+so we have an apache webpage that auto-display a formated message with music names..
+I have tried to figure out if there is *.list.txt or *.mp3 or folder..
+and I believe that by the name of our Network Sam, there must be a .key file or psw file in the sever...
+
+directory-level configuration file .. Apache..
+
+we tried every where through the .htaccess file
+but when we occasionally spotted the web directory scanner like dirBurster, we do find /b11/e/l/t/o/n/.htaccess 
+"the password is somewhere closer! try harder?"
+
+.. yes, somewhere closer!
