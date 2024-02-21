4333931fe19b4db22b7e971531c982feabaaf88a
ImRecc <46367069+ImRecc@users.noreply.github.com>
2024-02-21 19:50:46 +0800

    Create walkThrough.sh

---
+++ portswigger.net+apprentice/walkThrough.sh
@@ -0,0 +1,4 @@
+#!/accessControl
+
+vertical privilege escalation like change the role or authentical to view some path
+horizontal privilege escalation like a user could access other users' folder '
