0b4d87fe31c778583c1b7312907e3b4a910d5579
ImRecc <46367069+ImRecc@users.noreply.github.com>
2024-02-22 13:25:52 +0800

    Update walkThrough.sh

--- portswigger.net+apprentice/walkThrough.sh
+++ portswigger.net+apprentice/walkThrough.sh
@@ -2,3 +2,5 @@
 
 vertical privilege escalation like change the role or authentical to view some path
 horizontal privilege escalation like a user could access other users' folder '
+
+but all those happen only on verification...
