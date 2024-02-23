9b9bf2062fbe0e6a97d1b6bd5f563f36e278e6fa
ImRecc <46367069+ImRecc@users.noreply.github.com>
2024-02-23 19:14:47 +0800

    Update walkThrough.sh

--- portswigger.net+apprentice/walkThrough.sh
+++ portswigger.net+apprentice/walkThrough.sh
@@ -4,3 +4,6 @@ vertical privilege escalation like change the role or authentical to view some p
 horizontal privilege escalation like a user could access other users' folder '
 
 but all those happen only on verification...
+
+a interesting thing about two-factor verification: when a user passed login and verification, a token or mark granted,
+than, attacker can bypass two-factor verification too
