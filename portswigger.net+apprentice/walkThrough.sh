2fe334f212a1ac7a9457d33f8e280c8119ae9ecc
ImRecc <46367069+ImRecc@users.noreply.github.com>
2024-02-24 19:00:11 +0800

    Update walkThrough.sh

--- portswigger.net+apprentice/walkThrough.sh
+++ portswigger.net+apprentice/walkThrough.sh
@@ -8,4 +8,6 @@ but all those happen only on verification...
 a interesting thing about two-factor verification: when a user passed login and verification, a token or mark granted,
 than, attacker can bypass two-factor verification too
 
-!#/server-side authentical forgery, interesting way to bypass the verification
+#!/server-side authentical forgery, interesting way to bypass the verification
+
+#!/fileUploadVulnerabilities in apprentice level, zero verification or user-controllable verification not a prob anymore
