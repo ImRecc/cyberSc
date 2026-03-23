5c490eb2030a5e04b0e915b09c454f3b9a851863
ImRecc <46367069+ImRecc@users.noreply.github.com>
2026-03-23 15:44:02 +0800

    Update mainProcedures.sh

--- overTheWireMain/mainProcedures.sh
+++ overTheWireMain/mainProcedures.sh
@@ -1,2 +1,2 @@
 L0-L1. connect then cat, connect via ssh bandit0@bandit.labs.xxx.xxx -p xxxx
-l1-l2.
+l1-l2. to open file that names like "-", try cat ./- to make system understand we are not trying to add arg
