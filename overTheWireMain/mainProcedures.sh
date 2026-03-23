5a90017efbd104f0f9abee9328f423b21b6de4e3
ImRecc <46367069+ImRecc@users.noreply.github.com>
2026-03-23 16:18:01 +0800

    Update mainProcedures.sh

--- overTheWireMain/mainProcedures.sh
+++ overTheWireMain/mainProcedures.sh
@@ -1,2 +1,4 @@
 L0-L1. connect then cat, connect via ssh bandit0@bandit.labs.xxx.xxx -p xxxx
 l1-l2. to open file that names like "-", try cat ./- to make system understand we are not trying to add arg
+l2-l3. the space, usually recognized as seperate, but a simple tab key will make system know how to autofill. like cat ./--spaces\ in\ this\ filename-- 
+l3-l4
