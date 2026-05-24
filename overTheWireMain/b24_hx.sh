58ac639f7290a28037da36169d263a4cb4e7f446
ImRecc <46367069+ImRecc@users.noreply.github.com>
2026-05-24 20:03:22 +0800

    Create b24_hx.sh

---
+++ overTheWireMain/b24_hx.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# in linux's world, extensions mean nothing, as long as permitted allow, system gonna trying to run it, no matter a .jpg or .py
+# #!/usr/bin/python3 to run as python
+cat /etc/bandit_pass/bandit24 > /tmp/psw.txt
+# foget not to chmod 777 psw.txt to allow others write in
+# also the chmod +x hx.sh to add execution permit
