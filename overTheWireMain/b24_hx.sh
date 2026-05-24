fdae1f206f9d8611888cb4573d10b945a45d238f
ImRecc <46367069+ImRecc@users.noreply.github.com>
2026-05-24 20:15:22 +0800

    Update b24_hx.sh

--- overTheWireMain/b24_hx.sh
+++ overTheWireMain/b24_hx.sh
@@ -4,3 +4,5 @@
 cat /etc/bandit_pass/bandit24 > /tmp/psw.txt
 # foget not to chmod 777 psw.txt to allow others write in
 # also the chmod +x hx.sh to add execution permit
+# this is better than chmod 711 to modify more
+# chmod ugo +x == chmod a+x == chmod +x
